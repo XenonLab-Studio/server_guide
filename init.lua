8a97d7d9369a566866d5e2129f3c48c383456cda
XenonCoder <xenon77.dev@gmail.com>
2018-02-16 12:06:43 +0100

    fisex + button exit

--- init.lua
+++ init.lua
@@ -12,19 +12,21 @@
  of this license document, but changing it is not allowed.
 ]]--
 
-man = {}
+mt_welcome_server = {}
+
 -- SETTINGS 
 
-man.welcome = "Message...";
+mt_welcome_server.welcome = "Message...";
 
-man.players = {};
+mt_welcome_server.players = {};
 local worldpath = minetest.get_worldpath();
 
 minetest.register_on_joinplayer(function(player) 
-	local name = player:get_player_name(); if name == nil then return end 
+	local name = player:get_player_name(); if name == nil then return end
 	
 	-- read player inventory data
-	local form = "size [6,2] textarea[0,0;6.6,3.5;help;*** Form title ***;".. man.welcome.."]"
-	        -- minetest.chat_send_player(name, man.welcome);
-			minetest.show_formspec(name, "man:welcome", form)
+	local form = "size [6,2] textarea[0,0;6.6,3.5;help;*** Form title ***;".. mt_welcome_server.welcome.."]" ..
+	"button_exit[0,1;1,2;exit;Ok]"
+	        -- minetest.chat_send_player(name, form_msg.welcome);
+			minetest.show_formspec(name, "size [6,2] mt_welcome_server:welcome", form)
 end)
